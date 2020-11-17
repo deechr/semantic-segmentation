617f576883c8efe3ccca6de59603df45ab20602d
deechr <39114331+deechr@users.noreply.github.com>
2020-11-17 13:44:05 +0100

    speicherort daten geändert

--- config.py
+++ config.py
@@ -76,21 +76,21 @@ __C.TRAIN.FP16 = False
 __C.DATASET = AttrDict()
 #Cityscapes Dir Location
 __C.DATASET.CITYSCAPES_DIR = \
-  os.path.join(__C.ASSETS_PATH, 'data/Cityscapes')
-__C.DATASET.CITYSCAPES_CUSTOMCOARSE = \
-  os.path.join(__C.ASSETS_PATH, 'data/Cityscapes/autolabelled')
-__C.DATASET.CENTROID_ROOT = \
-  os.path.join(__C.ASSETS_PATH, 'uniform_centroids')
+  os.path.join(__C.ASSETS_PATH, '/content/drive/My Drive/Daten')
+#__C.DATASET.CITYSCAPES_CUSTOMCOARSE = \
+#  os.path.join(__C.ASSETS_PATH, 'data/Cityscapes/autolabelled')
+#__C.DATASET.CENTROID_ROOT = \
+#  os.path.join(__C.ASSETS_PATH, 'uniform_centroids')
 #SDC Augmented Cityscapes Dir Location
-__C.DATASET.CITYSCAPES_AUG_DIR = ''
+#__C.DATASET.CITYSCAPES_AUG_DIR = ''
 #Mapillary Dataset Dir Location
-__C.DATASET.MAPILLARY_DIR = os.path.join(__C.ASSETS_PATH, 'data/Mapillary/data')
+#__C.DATASET.MAPILLARY_DIR = os.path.join(__C.ASSETS_PATH, 'data/Mapillary/data')
 #Kitti Dataset Dir Location
-__C.DATASET.KITTI_DIR = ''
+#__C.DATASET.KITTI_DIR = ''
 #SDC Augmented Kitti Dataset Dir Location
-__C.DATASET.KITTI_AUG_DIR = ''
+#__C.DATASET.KITTI_AUG_DIR = ''
 #Camvid Dataset Dir Location
-__C.DATASET.CAMVID_DIR = ''
+#__C.DATASET.CAMVID_DIR = ''
 #Number of splits to support
 __C.DATASET.CITYSCAPES_SPLITS = 3
 __C.DATASET.MEAN = [0.485, 0.456, 0.406]
