f5c558066d9ccf0932f1c234c7562f959011b865
deechr <39114331+deechr@users.noreply.github.com>
2020-12-28 11:17:53 +0100

    Update config.py

--- config.py
+++ config.py
@@ -49,7 +49,7 @@ cfg = __C
 __C.GLOBAL_RANK = 0
 __C.EPOCH = 0
 # Absolute path to a location to keep some large files, not in this dir.
-__C.ASSETS_PATH = '/content/drive/My Drive/DataMining/large_asset_dir'
+__C.ASSETS_PATH = '/content/drive/My Drive/DataMining/alter/large_asset_dir'
 
 # Use class weighted loss per batch to increase loss for low pixel count classes per batch
 __C.BATCH_WEIGHTING = False
